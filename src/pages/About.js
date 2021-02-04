d7331fbf662529ae237309edeb60588cd778214a
Henri Brestea <henri.bresteau@gmail.com>
2021-02-04 10:52:37 +0100

    Pages componenet

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>À propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt magnam repellendus soluta consectetur id debitis impedit. Reprehenderit, autem. Quia earum quasi deleniti, maxime repellat dolorem corporis. Quod deleniti debitis, minus vero sed perferendis corrupti natus inventore autem labore voluptatum minima quaerat pariatur quo. Dignissimos, provident reprehenderit. Nobis eaque id recusandae dignissimos delectus, officiis facilis praesentium quia blanditiis voluptates sint aliquam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus corrupti eveniet, aliquam sit odio voluptates placeat consectetur voluptas ad nesciunt optio ipsum dicta facilis est magni quibusdam consequuntur. Non, sunt.</p>
+        </div>
+    );
+};
+
+export default About;
