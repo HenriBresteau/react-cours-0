90002f514956d17a26e06c23aa4e90eb6da485b1
henriB <henri.bretseau@gmail.com>
2021-07-14 15:30:19 +0200

    style on Editing

--- src/components/Article.js
+++ src/components/Article.js
@@ -17,7 +17,7 @@ const Article = ({ article }) => {
   const handleEdit = () => {
     const data = {
       author: article.author,
-      content: editedContent,
+      content: editedContent ? editedContent : article.content,
       date: article.date,
     };
     axios.put("http://localhost:3003/articles/" + article.id, data).then(() => {
@@ -25,7 +25,10 @@ const Article = ({ article }) => {
     });
   };
   return (
-    <div className="article">
+    <div
+      className="article"
+      style={{ background: isEditing ? "#61dafbb5" : "white" }}
+    >
       <div className="card-header">
         <h3> {article.author} </h3>
         <em>Posté le {dateParser(article.date)} </em>
