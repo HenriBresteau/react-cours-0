ead9b4d7508d2f6ec3277c6919ef7a2fa1a4bf2c
henriB <henri.bretseau@gmail.com>
2021-07-14 15:15:22 +0200

    EditContent

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,11 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
 
 const Article = ({ article }) => {
+  const [isEditing, setisEditing] = useState(false);
+  const [editedContent, setEditedContent] = useState("");
+
   const dateParser = (date) => {
     let newDate = new Date(date).toLocaleDateString("fr-FR", {
       year: "numeric",
@@ -9,15 +14,37 @@ const Article = ({ article }) => {
     });
     return newDate;
   };
+  const handleEdit = () => {
+    const data = {
+      author: article.author,
+      content: editedContent,
+      date: article.date,
+    };
+    axios.put("http://localhost:3003/articles/" + article.id, data).then(() => {
+      setisEditing(false);
+    });
+  };
   return (
     <div className="article">
       <div className="card-header">
         <h3> {article.author} </h3>
         <em>Posté le {dateParser(article.date)} </em>
       </div>
-      <p> {article.content} </p>
+      {isEditing ? (
+        <textarea
+          onChange={(e) => setEditedContent(e.target.value)}
+          autofocus
+          defaultValue={editedContent ? editedContent : article.content}
+        ></textarea>
+      ) : (
+        <p> {editedContent ? editedContent : article.content} </p>
+      )}
       <div className="btn-container">
-        <button>Edit</button>
+        {isEditing ? (
+          <button onClick={handleEdit}>Valider</button>
+        ) : (
+          <button onClick={() => setisEditing(true)}>Edit</button>
+        )}
         <button>Delete</button>
       </div>
     </div>
